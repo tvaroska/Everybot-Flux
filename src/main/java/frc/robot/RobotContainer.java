2c9dee414f8b0f7b438a1faecc5594a97e227755
yapplejack <jacklf@umich.edu>
2025-01-28 00:20:15 -0500

    minor correction

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -143,7 +143,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
     public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
+    // The selected command will be run in autonomous
     return m_chooser.getSelected();
   }
 }
